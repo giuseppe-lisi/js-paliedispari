b3fad25d0a0aebcb69a457c4f6e7ad49c9cc2193
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-05 19:24:59 +0100

    isPalindrome è ora interattivo

--- Palindrome/isPalindrome.js
+++ Palindrome/isPalindrome.js
@@ -1,34 +1,13 @@
-/*
-Per determinare se una parola è palindroma, in JS, 
-non esiste un metodo che sia in grado di fare tutto 
-il lavoro al posto nostro. Tuttavia, possiamo utilizzare 
-due escamotage per ottenere il risultato voluto:
-
-1 - Suddividiamo la stringa in modo tale che
-    ogni carattere diventi un elemento di un array.
-    Reversiamo quindi l'array e facciamo poi il join degli
-    elementi per farlo tornare una stringa
-
-2 - Sfruttiamo un for loop che "legge" uno ad uno i
-    caratteri della stringa partendo dall'ultimo
-    e li salva progressivamente all'interno di una nuova stringa. 
-    Controlliamo quindi se la nuova stringa, invertita, 
-    è uguale a quella di partenza
-*/
-
-// Metodo 1
-
-// chiedo all'utente di inserire una parola
-// let userWord = prompt("Inserisci una parola, io controllerò se è palindroma");
-
 // prende una stringa e restituisce vero se è palindroma altrimenti falso
 function isPalindromeWithArray(str) {
     let strReversed = str.split("").reverse().join("");
     if (strReversed == str) {
-        return true;
+        return document.getElementById('output1').value = "true";
     }
-    return false;
+    return document.getElementById('output1').value = "false";
 }
+
+
 
 // prende una stringa e restituisce vero se è palindroma altrimenti falso
 function isPalindromeWithFor(str) {
@@ -37,7 +16,7 @@ function isPalindromeWithFor(str) {
         strReversed += str[i];
     }
     if (strReversed == str) {
-        return true;
+        return document.getElementById('output2').value = "true";;
     }
-    return false;
+    return document.getElementById('output2').value = "false";;
 }
