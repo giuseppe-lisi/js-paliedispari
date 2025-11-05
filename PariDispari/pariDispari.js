ddba88b6f5c8d1acc3c5cae6053f5ed9c189c690
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-05 18:38:43 +0100

    data una ripulita a pariDispari

--- PariDispari/pariDispari.js
+++ PariDispari/pariDispari.js
@@ -4,10 +4,16 @@ function getRandomInRangeInclusive(min, max) {
 }
 
 let computerNumber = getRandomInRangeInclusive(1,5);
+let userChoice = "";
+let userNumber;
+// utente inserisce scelta tra pari o dispari e numero
+while (userChoice != "pari" && userChoice != "dispari") {
+    userChoice = prompt("Scegli tra 'pari' o 'dispari'").toLowerCase();
+}
 
-// utente inserisce scelta e numero
-let userChoice = prompt("Scegli tra pari e dispari")
-let userNumber = parseInt(prompt("Scegli un numero compreso tra 1 e 5"))
+while (!(userNumber >= 1 && userNumber <= 5) || isNaN(userNumber)) {
+    userNumber = parseInt(prompt("Scegli un numero compreso tra 1 e 5"))
+}
 
 // determina se la somma dei due numeri è pari o dispari
 function isSumEvenOrOdd(num1, num2) {
@@ -22,7 +28,7 @@ let risultato = isSumEvenOrOdd(computerNumber, userNumber);
 
 // determina se ha vinto l'utente o la CPU
 if (risultato == userChoice) {
-    alert("Hai vinto!");
+    alert(`La CPU ha scelto ${computerNumber}. Hai vinto!`);
 } else {
-    alert("Hai perso!");
+    alert(`La CPU ha scelto ${computerNumber}. Hai perso!`);
 }
